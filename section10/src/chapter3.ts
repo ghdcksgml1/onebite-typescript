723a1f5b211a71228a785ea0eb6cb5c3c47562d8
ghdcksgml1 <ghdcksgml2@naver.com>
2024-03-01 15:01:46 +0900

    feat: section10

---
+++ section10/src/chapter3.ts
@@ -0,0 +1,31 @@
+/**
+ * Exclude<T, U>
+ * -> 제외하다, 추방하다
+ * -> T에서 U를 제거하는 타입
+ */
+type Exclude<T, U> = T extends U ? never : T;
+type A = Exclude<string | boolean, boolean>; // string
+
+/**
+ * Extract<T, U>
+ * -> T에서 U를 추출하는 타입
+ */
+type Extract<T, U> = T extends U ? T : never;
+type B = Extract<string | boolean, boolean>; // boolean
+
+/**
+ * ReturnType
+ * -> 함수의 반환값 타입을 추출하는 타입
+ */
+type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : never;
+
+function funcA() {
+    return "hello";
+}
+
+function funcB() {
+    return 10;
+}
+
+type ReturnA = ReturnType<typeof funcA>
+type ReturnB = ReturnType<typeof funcB>
